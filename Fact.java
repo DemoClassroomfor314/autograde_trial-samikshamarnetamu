fa4246d1fef305c9ab52b37597e0801fee336598
samikshamarnetamu <samikshamarne@tamu.edu>
2021-01-31 17:15:32 -0600

    Update Fact.java

--- Fact.java
+++ Fact.java
@@ -2,8 +2,13 @@ import java.util.Scanner;
    public class Fact {
       public static void main(String args[]){
          int i, factorial=1;
+         System.out.println("Enter number:");
          Scanner sc = new Scanner(System.in);
-         int number = sc.nextInt();
+         int number;
+         if(sc.hasNextInt())
+            number = sc.nextInt();
+         else
+            number = 0;
          for(i = 1; i<=number; i++) {
             factorial *= i;
          }
